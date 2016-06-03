1bb948cc23068d322979edf9c2474bd83727cf29
David Graf <davidagraf@gmail.com>
2016-06-03 15:14:05 +0200

    don't update openssl

--- brew.sh
+++ brew.sh
@@ -47,7 +47,7 @@ brew install node
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
+#brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 #brew install homebrew/php/php56 --with-gmp
 
